d194486fd5eb1047da9e64ee56a382c5b558214d
Diego Chavez <ingdiegochavez18@gmail.com>
2022-03-17 14:36:11 -0500

    feat/Change-integration-of-dependencies
    ** Change from cdn to dependencies in yarn:
    bootstrap5, popper, animateCSS, sweetAlert2.

--- src/main.js
+++ src/main.js
@@ -1,4 +1,10 @@
 import { createApp } from 'vue'
+import "bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "font-awesome/css/font-awesome.min.css";
+import "animate.css/animate.min.css";
+import "sweetalert2/dist/sweetalert2.min.css";
 import App from './App.vue'
+
 
 createApp(App).mount('#app')
