807f0079683c418bca35b4b5a970389433265465
Diego Chavez <ingdiegochavez18@gmail.com>
2022-03-17 14:53:11 -0500

    core/config-electron-with-vite

--- vite.config.js
+++ vite.config.js
@@ -2,8 +2,8 @@ import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 const path = require("path");
 
-// https://vitejs.dev/config/
+
 export default defineConfig({
-	//base: path.resolve(__dirname, "./dist/"),
+	base: path.resolve(__dirname, "./dist/"),
 	plugins: [vue()],
 });
