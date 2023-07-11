ea158a7b657a70f0123d7058edb1b3bcc32f7659
Diego-18 <ingdiegochavez18@gmail.com>
2023-07-10 23:53:19 -0500

    add-dockerfiles

--- vite.config.js
+++ vite.config.js
@@ -2,8 +2,10 @@ import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 const path = require("path");
 
-
 export default defineConfig({
 	//base: path.resolve(__dirname, "./dist/"),
 	plugins: [vue()],
+	server: {
+		port: 3000,
+	},
 });
